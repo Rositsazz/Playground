77b3d5d49909552ece026f740de88b2f05548872
Rositsazz <ross_zz@mail.bg>
2017-06-08 00:18:03 +0300

    gitignore and clean up elevator code

--- week01/elevator.py
+++ week01/elevator.py
@@ -7,22 +7,20 @@ def elevator_steps(people_weight, floors, elevator_floors,
         return 0
 
     if sorted(floors)[-1] > elevator_floors:
-        raise Exception("The elevator max floor is {}".format(elevator_floors))
+        raise Exception("Max floor is {}".format(elevator_floors))
 
     steps = 0
     while(len(people_weight) > 0):
         for i in range(max_persons):
             if sum(people_weight[:max_persons-i]) > max_weight:
                 if len(people_weight[:max_persons-i]) == 1:
-                    raise Exception("Elevator overweight!")
+                    raise Exception("Overweight!")
                 continue
             else:
-                current_list = people_weight[:max_persons-i]
-                current_floors = floors[:max_persons-i]
                 people_weight = people_weight[max_persons-i:]
                 floors = floors[max_persons-i:]
                 break
 
-        steps += len([list(j) for i, j in groupby(current_floors)]) + 1
+        steps += len([list(j) for i, j in groupby(floors[:max_persons-i])]) + 1
 
     return steps
